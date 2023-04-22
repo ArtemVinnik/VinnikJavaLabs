93c4a2d3f98d63f09603e91b5547a8aae09397f2
Artem Vinnik <Artem_Vinnik@epam.com>
2023-04-22 18:37:42 +0300

    Implement matrix service

--- src/main/java/edu/nmu/vinnik/App.java
+++ src/main/java/edu/nmu/vinnik/App.java
@@ -1,9 +1,28 @@
 package edu.nmu.vinnik;
 
-public class App 
+import edu.nmu.vinnik.lab2.MatrixService;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        MatrixService matrixService = new MatrixService();
+
+        int[][] matrix = matrixService.create();
+
+        matrixService.printMatrix(matrix);
+
+        int min = matrixService.getMinValue(matrix);
+        int max = matrixService.getMaxValue(matrix);
+
+        double arithmeticMean = matrixService.getArithmeticMean(matrix);
+        double geometricMean = matrixService.getGeometricMean(matrix);
+
+        System.out.println("Min value of the matrix: " + min);
+        System.out.println("Max value of the matrix: " + max);
+        System.out.println("Arithmetic mean: " + arithmeticMean);
+        System.out.println("Geometric mean: " + geometricMean);
     }
 }
