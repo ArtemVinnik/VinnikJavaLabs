4e5a6e73a30bd54be956a11ec4022aa1f4ef1223
Artem Vinnik <Artem_Vinnik@epam.com>
2023-05-16 15:14:29 +0300

    Add Student model

---
+++ src/main/java/edu/nmu/vinnik/lab5/models/Student.java
@@ -0,0 +1,13 @@
+package edu.nmu.vinnik.lab5.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Student {
+    private UUID id;
+    private String surname;
+    private String name;
+    private String patronymicName;
+    private Date dateOfBirth;
+    
+}
